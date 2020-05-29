0d6e29a527697f7b795886a640482ed6be154bc0
Adriano <adriano.militao@yahoo.com.br>
2020-05-29 12:26:10 -0300

    commit

--- nagiosmail/src/main/java/br/com/atacadao/nagiosmail/Main.java
+++ nagiosmail/src/main/java/br/com/atacadao/nagiosmail/Main.java
@@ -3,19 +3,28 @@ package br.com.atacadao.nagiosmail;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import br.com.atacadao.nagiosmail.service.CorreioService;
-import br.com.atacadao.nagiosmail.service.impl.CorreioCsc;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
 
 		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(
 				Main.class.getPackage().getName());
 
-		CorreioService correio = applicationContext.getBean(CorreioCsc.class);
-		
-		correio.enviar(args);
+		String nomeDaClasse = "CorreioCsc";
+
+		try {
+			Class<?> classe = Class.forName("br.com.atacadao.nagiosmail.service.impl." + nomeDaClasse);
+
+			CorreioService c = (CorreioService) classe.newInstance();
+
+			CorreioService correio = applicationContext.getBean(c.getClass());
+
+			correio.enviar(args);
+
+		} catch (Exception e) {
+			throw new RuntimeException("Encontrou erro " + e);
+		}
 
 		applicationContext.close();
 
